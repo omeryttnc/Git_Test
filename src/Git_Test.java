4039b232ddd18cc85429904b0337e7a042226ac8
omeryttnc <omeryttnc@gmail.com>
2020-07-15 21:49:03 +0300

    test no: 3

--- src/Git_Test.java
+++ src/Git_Test.java
@@ -2,5 +2,7 @@ public class Git_Test {
     public static void main(String[] args) {
         System.out.println("Git message");
         System.out.println("GitHub message");
+        System.out.println("Git message 2 ");
+
     }
 }
